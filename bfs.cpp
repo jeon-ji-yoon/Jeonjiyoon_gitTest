dfae8e7d369811b63e8d5b4010bc1e9c71e8e3cb
Jeon Ji Yoon <rebegin15@naver.com>
2020-12-29 14:08:16 +0900

    1229

--- bfs.cpp
+++ bfs.cpp
@@ -8,44 +8,37 @@ int number = 7;
 bool c[8];
 vector<int> a[8];
 
-void bfs(int start){
-	queue<int> q;
-	q.push(start);
-	c[start] = true;
-	while(!q.empty()){
-		int x = q.front();
-		q.pop();
-		printf("%d", x);
-		for(int i = 0; i<a[x].size();i++){
-			int y = a[x][i];
-			if(!c[y] ){
-				q.push(y);
-				c[y] = true;
-			}
-			
-		}
+void dfs(int x){
+	if(  !c[x]){
+		return;
+	}
+	c[x] = true;
+	cout<<x;
+	for(   int i =0; i< a[x].size() ; i++  ){
+		int y = a[x][i];
+		dfs[y];
 	}
 }
 
 int main(void){
 	a[1].push_back(2);
 	a[1].push_back(3);
-	a[2].push_back(1);
+	//a[2].push_back(1);
 	a[2].push_back(3);
 	a[2].push_back(4);
 	a[2].push_back(5);
-	a[3].push_back(1);
-	a[3].push_back(2);
+	//a[3].push_back(1);
+	//a[3].push_back(2);
 	a[3].push_back(6);
 	a[3].push_back(7);
-	a[4].push_back(2);
+	//a[4].push_back(2);
 	a[4].push_back(5);
-	a[5].push_back(2);
-	a[5].push_back(4);
-	a[6].push_back(3);
+	//a[5].push_back(2);
+	//a[5].push_back(4);
+	//a[6].push_back(3);
 	a[6].push_back(7);
-	a[7].push_back(3);
-	a[7].push_back(6);
+	//a[7].push_back(3);
+	//a[7].push_back(6);
 	bfs(1);
 	return 0;
 }
